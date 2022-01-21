cd135280d6e9c834979b07d5ee801f66a80c540f
Rebecca Sissman <rsissman15@aol.com>
2022-01-21 12:59:49 -0500

    My Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded',()=>{
+   let form =document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    handleToDoList(e.target.new_task_description.value);
+    //form.reset();
+  });
 });
+
+function handleToDoList(todo){
+  let li= document.createElement('li');
+  let btn=document.createElement('button');
+  btn.addEventListener('click',handleDelete)
+  btn.textContent=' x'
+  li.textContext=todo;
+  li.appendChild(btn);
+  console.log(li);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
