21a0a149244a1ec03706708900afbf37e242a41b
Rebecca Sissman <rsissman15@aol.com>
2022-01-21 13:35:07 -0500

    My Project Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded',()=>{
-   let form =document.querySelector('form').addEventListener('submit',(e)=>{
+   let form =document.querySelector('form')
+   form.addEventListener('submit',(e)=>{
     e.preventDefault();
+    //console.log(e.target.new_task_description.value);
     handleToDoList(e.target.new_task_description.value);
-    //form.reset();
+    form.reset();
   });
 });
 
 function handleToDoList(todo){
+  //console.log(`I am here ${todo}`);
   let li= document.createElement('li');
   let btn=document.createElement('button');
   btn.addEventListener('click',handleDelete)
   btn.textContent=' x'
-  li.textContext=todo;
+  //li.textContext=todo;
+  li.append(todo);
   li.appendChild(btn);
   console.log(li);
   document.querySelector("#tasks").appendChild(li);
